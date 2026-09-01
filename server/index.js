7fff22f810b9797023ee14e8ffc8630374a52b6f
murasakijyuutann <yuuka.hayase1030@gmail.com>
2026-09-01 22:15:47 +0900

    learning continues

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+// express is a web framework for node.js
+
 const { createServer } = require('http');
+/* { createServer } from 'http' : Node's built-in HTTP server module. We destructure just the one function we need out of it,
+ same idea as importing a single static method rather than a whole class in Java. */
+
 const { Server } = require('socket.io');
+/* { Server } from 'socket.io' : Socket.IO's main class. (capitalised by convention), 
+    which we'll new up in a second. This is the Socket.IO engine itself. */
+
 
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
     cors: { origin: 'http://localhost:5173' },
 })
+
